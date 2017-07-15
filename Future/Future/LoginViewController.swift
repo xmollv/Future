340fd207fbd6a95656cb536f52407d6ebf5de358
Xavi Moll <xmollv@gmail.com>
2017-07-15 15:43:58 +0200

    Rounded the login button and changed the status bar to light

--- Future/Future/LoginViewController.swift
+++ Future/Future/LoginViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    //MARK:- IBOutlets
     @IBOutlet private var nameTextField: UITextField!
     @IBOutlet private var logInButton: UIButton!
     @IBOutlet fileprivate var bottomScrollViewConstraint: NSLayoutConstraint!
     
+    //MARK:- View controller lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self,
@@ -21,9 +23,23 @@ class LoginViewController: UIViewController {
                                                name: .UIKeyboardWillChangeFrame,
                                                object: nil)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // The geometry is final here, we can safely round the button
+        roundLoginButton()
+    }
+    
+    //MARK:- Private methods
+    private func roundLoginButton() {
+        let height = logInButton.bounds.height
+        logInButton.layer.cornerRadius = height/2
+        logInButton.layer.masksToBounds = true
+    }
 
 }
 
+//MARK: UIKeyboardWillChangeFrame extension
 extension LoginViewController {
     @objc fileprivate func keyboardChanged(notification: NSNotification) {
         KeyboardManager.keyboardNotification(notification: notification, constraint: bottomScrollViewConstraint, view: self.view)
