1edc29fe00406ff32e9951b000e8ae27c7d6f75d
Xavi Moll <xmollv@gmail.com>
2017-07-15 15:03:30 +0200

    Created the base of the project

---
+++ Future/Future/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Future
+//
+//  Created by Xavi Moll on 15/07/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
