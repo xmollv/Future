926a0f00baec6a2c89bf491eae8bd206634d57c3
Xavi Moll <xmollv@gmail.com>
2017-07-15 15:16:02 +0200

    Created the basic UI for the login screen

---
+++ Future/Future/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Future
+//
+//  Created by Xavi Moll on 15/07/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
