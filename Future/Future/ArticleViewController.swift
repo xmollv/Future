f4a8d101e8e915883ec88986bb592085edad79bb
Xavi Moll <xmollv@gmail.com>
2017-07-16 12:43:52 +0200

    Created the basic UI for the article details view controller

--- Future/Future/ArticleViewController.swift
+++ Future/Future/ArticleViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ArticleViewController: UIViewController {
     
+    //MARK: IBOutlets
+    @IBOutlet private var articleImageView: UIImageView!
+    @IBOutlet private var articleScrollView: UIScrollView!
+    @IBOutlet private var articleTitleLabel: UILabel!
+    @IBOutlet private var articleSubtitleLabel: UILabel!
+    @IBOutlet private var articleTextView: UITextView!
+    
+    //MARK: Class properties
     var dataProvider: DataProvider!
     var articleId: Int!
 
+    //MARK: View controller lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Removes the unwanted padding for the UITextView
+        articleTextView.textContainerInset = UIEdgeInsets.zero
+        articleTextView.textContainer.lineFragmentPadding = 0
+        
         fetchData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Used the scrollView to create more of a 'card' style UI leaving the
+        // article image on the background when scrolling
+        let imageHeight = articleImageView.bounds.height
+        articleScrollView.contentInset = UIEdgeInsetsMake(imageHeight, 0, 0, 0)
+    }
+    
+    //MARK: Private methods
     private func fetchData() {
         dataProvider.getSingle(.newsDetails(id: articleId)) { (result: Result<Article>) in
             dump(result)
